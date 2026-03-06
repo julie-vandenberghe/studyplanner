42d00e719f03d07c444604ec9f262fbe630f1f50
Clement-Szewczyk <clement.szewczyk@gmail.com>
2026-03-06 11:33:30 +0100

    Refactor name package

--- src/main/java/com/studyplanner/StudyplannerApplication.java
+++ src/main/java/com/studyplanner/StudyplannerApplication.java
@@ -1,4 +1,4 @@
-package com.studyplanner.studyplanner;
+package com.studyplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
